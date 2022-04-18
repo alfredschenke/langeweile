b73de5b722005c4956eff99df708f6dc03ac8e23
David Enke <david@enke.dev>
2022-04-18 15:36:36 +0200

    fix: build script

--- rollup.config.ts
+++ rollup.config.ts
@@ -22,73 +22,6 @@ const isWatch = process.argv.includes('--watch');
 // rollup warnings to ignore
 const SKIP_WARNINGS = ['CIRCULAR_DEPENDENCY', 'EVAL', 'THIS_IS_UNDEFINED', 'UNKNOWN_OPTION', 'UNRESOLVED_IMPORT'];
 
-// always required plugins
-const PLUGINS_BASE: Plugin[] = [
-  del({
-    runOnce: true,
-    targets: 'dist',
-  }),
-  copy({
-    targets: [
-      {
-        src: 'src/_redirects',
-        dest: 'dist/',
-      },
-      {
-        src: 'src/assets/*',
-        dest: 'dist/assets',
-      },
-      {
-        src: 'node_modules/@fontsource/roboto-condensed/files/*',
-        dest: 'dist/assets/fonts/roboto-condensed',
-      },
-    ],
-  }),
-  {
-    name: 'store-image-list',
-    generateBundle: async () => {
-      const sources = await glob('src/assets/images/memory/*.jpg');
-      const images = sources.map(source => source.replace('src', ''));
-      await writeFile('dist/assets/images/memory.json', JSON.stringify(images));
-    },
-  } as Plugin,
-  resolve(),
-  html({
-    meta: [{ name: 'viewport', content: 'width=device-width, user-scalable=no' }],
-    publicPath: '/',
-    title: 'Alfreds Memory',
-  }),
-  sass({ processor: css => postcss([autoprefixer]).process(css, { from: undefined }) }),
-  typescript(),
-];
-
-// plugins for production
-const PLUGINS_PROD: Plugin[] = [
-  minifyHTML(),
-  terser({
-    ecma: 2020,
-    module: true,
-  }),
-];
-
-// plugins for watch mode
-const PLUGINS_WATCH: Plugin[] = [
-  serve({
-    contentBase: 'dist',
-    historyApiFallback: true,
-    port: 3000,
-  }),
-];
-
-// combine relevant plugins
-let PLUGINS_COMBINED: Plugin[] = [...PLUGINS_BASE];
-if (isProd) {
-  PLUGINS_COMBINED = [...PLUGINS_COMBINED, ...PLUGINS_PROD];
-}
-if (isWatch) {
-  PLUGINS_COMBINED = [...PLUGINS_COMBINED, ...PLUGINS_WATCH];
-}
-
 export default defineConfig({
   input: 'src/index.ts',
   output: { dir: 'dist' },
@@ -98,6 +31,57 @@ export default defineConfig({
     }
     next(message);
   },
-  plugins: PLUGINS_COMBINED,
+  plugins: [
+    del({
+      runOnce: true,
+      targets: 'dist',
+    }),
+    copy({
+      targets: [
+        {
+          src: 'src/_redirects',
+          dest: 'dist/',
+        },
+        {
+          src: 'src/assets/*',
+          dest: 'dist/assets',
+        },
+        {
+          src: 'node_modules/@fontsource/roboto-condensed/files/*',
+          dest: 'dist/assets/fonts/roboto-condensed',
+        },
+      ],
+    }),
+    {
+      name: 'store-image-list',
+      generateBundle: async () => {
+        const sources = await glob('src/assets/images/memory/*.jpg');
+        const images = sources.map(source => source.replace('src', ''));
+        await writeFile('dist/assets/images/memory.json', JSON.stringify(images));
+      },
+    } as Plugin,
+    resolve(),
+    html({
+      meta: [{ name: 'viewport', content: 'width=device-width, user-scalable=no' }],
+      publicPath: '/',
+      title: 'Alfreds Memory',
+    }),
+    sass({ processor: css => postcss([autoprefixer]).process(css, { from: undefined }) }),
+    typescript(),
+
+    isProd && minifyHTML(),
+    isProd &&
+      terser({
+        ecma: 2020,
+        module: true,
+      }),
+
+    isWatch &&
+      serve({
+        contentBase: 'dist',
+        historyApiFallback: true,
+        port: 3000,
+      }),
+  ],
   preserveEntrySignatures: 'strict',
 });
